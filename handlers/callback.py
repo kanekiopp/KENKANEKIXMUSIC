5082beb24f99cd0a74c472de88f7a3a555bf5e81
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2021-11-19 21:36:16 +0530

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -19,8 +19,8 @@ from config import (
 @Client.on_callback_query(filters.regex("cbstart"))
 async def cbstart(_, query: CallbackQuery):
     await query.edit_message_text(
-        f"""✨ **𝚆𝙴𝙻𝙲𝙾𝙼𝙴 [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
-🔥 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) [Cᴀɴ](https://te.legra.ph/file/a045aefa994cd73320fa0.jpg), Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Oᴘ Gʀᴏᴜᴩ Vᴏɪᴄᴇ Cʜᴀᴛ💖. Dᴇᴠᴇʟᴏᴩᴇᴅ Bʏ @DARKAMAN !**""",
+        f"""✨ **𝐖𝐄𝐋𝐂𝐎𝐌𝐄 [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
+🔥 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) [Cᴀɴ](https://te.legra.ph/file/a045aefa994cd73320fa0.jpg), 𝐏𝐋𝐀𝐘 𝐌𝐔𝐒𝐈𝐂 𝐈𝐍 𝐘𝐎𝐔𝐑 𝐎𝐏 𝐆𝐑𝐎𝐔𝐏 𝐕𝐎𝐈𝐂𝐄 𝐂𝐇𝐀𝐓💖.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
