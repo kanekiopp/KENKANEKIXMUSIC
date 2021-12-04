f5bee72638a387771b6703058dd714499581835e
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2021-12-05 00:45:55 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -609,7 +609,7 @@ async def play(_, message: Message):
     file_path = await converter.convert(youtube.download(url))
     for x in callsmusic.pytgcalls.active_calls:
             ACTV_CALLS.append(int(x.chat_id))
-        if int(chat_id) not in ACTV_CALLS:
+        if int(chat_id) in ACTV_CALLS:
         position = await queues.put(chat_id, file=file_path)
         qeue = que.get(chat_id)
         s_name = title
