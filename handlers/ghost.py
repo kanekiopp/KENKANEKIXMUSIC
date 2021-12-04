e20d3864037ff03f2f19f179e779cc26edb0b1cb
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2021-12-04 17:20:13 +0530

    Update ghost.py

--- handlers/ghost.py
+++ handlers/ghost.py
@@ -19,6 +19,27 @@ TIME_DURATION_UNITS = (
     ("sec", 1),
 )
 
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - START_TIME)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    stats = f'''
+Uptime: {get_readable_time((bot_uptime))}
+CPU: {cpu}%
+RAM: {mem}%
+Disk: {disk}%'''
+    return stats
+
+
+@Client.on_message(filters.command(["ping", "ping@{BOT_USERNAME}"]))
+async def ping(_, message):
+    uptime = await bot_sys_stats()
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo="{ALIVE_IMG}",
+        caption=">> Pong!"
+    )
 
 @Client.on_message(filters.command(["uptime", f"uptime@{BOT_USERNAME}"]))
 async def get_uptime(client: Client, message: Message):
